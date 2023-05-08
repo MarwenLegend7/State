587e9d0a674a26c80dec827c2932a2f3bca5aaee
Legend <marwen.aouinti92@gmail.com>
2023-05-08 22:56:29 +0100

    State First Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React from 'react';
+import Profile from './component/Profile';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {isVisible: false}
+  }
+
+  toggleVisibility = () => {
+    this.setState({isVisible: !this.state.isVisible})
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <div>
+        
+        <button class="btn btn-primary" onClick={this.toggleVisibility}>
+          {this.state.isVisible ? "Hide Profile" : "Show Profile"}
+        </button>
+      </div>
+      {this.state.isVisible && <Profile />}
+    </div>
+  );
+}
+}
+
+export default App;
